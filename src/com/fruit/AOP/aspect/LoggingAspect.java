1a3593261569b258762a3968f0cc54a304ed9a9b
SHIAN HUANG <shian.huang@ftlinc.com>
2014-08-06 13:40:13 -0500

    Used wildcard in @Before and used @Pointcut
    For method parameters:
    .. : means any number of parameters, from 0 to any number
    * : means one or more number of parameters

--- src/com/fruit/AOP/aspect/LoggingAspect.java
+++ src/com/fruit/AOP/aspect/LoggingAspect.java
@@ -2,13 +2,23 @@ package com.fruit.AOP.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before(value = "execution(public String getName())")
+	@Before("allGetters()")
 	public void LoggingAdvice() {
 		
 		System.out.println("Advice run. Get Method called");
 	}
+	
+	@Before("allGetters()")
+	public void secondAdvice() {
+		
+		System.out.println("Second Advice executed");
+	}
+	
+	@Pointcut("execution(public * get*(..))")
+	public void allGetters(){}
 }
