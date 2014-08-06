052a0ce6b0422bd03492b9cc9eb856ab6b5bd46c
Shian <shianhuang2008@gmail.com>
2014-08-05 22:37:01 -0500

    added classes and spring.xml

---
+++ src/com/fruit/AOP/model/Triangle.java
@@ -0,0 +1,15 @@
+package com.fruit.AOP.model;
+
+public class Triangle {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
